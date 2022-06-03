5778ef8ed6acb5299b5abb3faf61225b49df8727
Kasper Vrind <kasper.vrind@minbzk.nl>
2022-06-03 11:42:09 +0200

    That took some time to fix this error. Now behat runs

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
 ];
