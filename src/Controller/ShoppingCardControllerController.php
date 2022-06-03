a5e67e4b5f08b196a1c685f05174cff0b2bb1fa3
Kasper Vrind <kasper.vrind@minbzk.nl>
2022-06-03 12:40:50 +0200

    Fixed the card to render the page

--- src/Controller/ShoppingCardControllerController.php
+++ src/Controller/ShoppingCardControllerController.php
@@ -5,32 +5,32 @@ namespace Coolblue\Interview\Controller;
 use Coolblue\Interview\Entity\ShoppingCart;
 use Coolblue\Interview\Repository\ShoppingCartRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ShoppingCardControllerController extends AbstractController
 {
     private ShoppingCart $cart;
 
-    public function __construct()
-    {
-        $this->cart = (new ShoppingCartRepository())->getShoppingCart(($_GET['cartid']) ?: 1);
-    }
-
     #[Route('/card', name: 'app_shopping_card_controler')]
-    public function index(): string
+    public function index(Request $request): Response
     {
-        return $this->renderCard();
+        return $this->renderCard($request->get('cartid', 1));
     }
 
-    public function renderCard(): string
+    public function renderCard($cartId): Response
     {
+        // TODO hacky but alright for now
+        $this->cart = (new ShoppingCartRepository())->getShoppingCart($cartId);
+
         ob_start();
 
-        require __DIR__.'/../template/cart.tpl';
+        require __DIR__.'/../../template/cart.tpl';
 
         $result = ob_get_contents();
         ob_clean();
 
-        return $result;
+        return new Response($result);
     }
 }
